4ead1d8ec42947a629142b01100cbf69345329bb
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2018-03-23 12:57:06 +0530

    setup GUI

---
+++ app/src/main/java/ml/sziraqui/navwi/POJO/WiDevice.java
@@ -0,0 +1,35 @@
+package ml.sziraqui.navwi.POJO;
+
+/**
+ * Created by sziraqui on 23/3/18.
+ */
+
+public class WiDevice {
+    private String name;
+    private Double signalStrength;
+    private String hwAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSignalStrength() {
+        return signalStrength;
+    }
+
+    public void setSignalStrength(Double signalStrength) {
+        this.signalStrength = signalStrength;
+    }
+
+    public String getHwAddress() {
+        return hwAddress;
+    }
+
+    public void setHwAddress(String hwAddress) {
+        this.hwAddress = hwAddress;
+    }
+}
---
+++ app/src/main/java/ml/sziraqui/navwi/adapters/WiDeviceAdapter.java
@@ -0,0 +1,59 @@
+package ml.sziraqui.navwi.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ml.sziraqui.navwi.POJO.WiDevice;
+import ml.sziraqui.navwi.R;
+
+/**
+ * Created by sziraqui on 23/3/18.
+ */
+
+public class WiDeviceAdapter extends RecyclerView.Adapter<WiDeviceAdapter.WiDeviceHolder> {
+
+    Context context;
+    List<WiDevice> devices;
+
+    public class WiDeviceHolder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView name;
+        TextView value;
+        public WiDeviceHolder(View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.widevice_iv);
+            name = itemView.findViewById(R.id.widevice_name_tv);
+            value = itemView.findViewById(R.id.widevice_signal_value_tv);
+        }
+
+        public void bindHolder(WiDevice device) {
+            name.setText(device.getName());
+            value.setText(""+device.getSignalStrength());
+        }
+    }
+
+    @Override
+    public WiDeviceAdapter.WiDeviceHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.wi_device_item, parent, false));
+        return new WiDeviceHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(WiDeviceAdapter.WiDeviceHolder holder, int position, List payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+        holder.bindHolder(devices.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return devices.size();
+    }
+}
